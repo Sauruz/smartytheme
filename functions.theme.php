ef0acbfea5c603d61f2ba16d908f4d1afef3cc8b
Guido <G.rus@de-rus.nl>
2016-07-07 19:02:10 +0700

    Custom fields

--- functions.theme.php
+++ functions.theme.php
@@ -1,5 +1,43 @@
 <?php
 
+/**
+ * Include the TGM_Plugin_Activation class.
+ */
+require_once dirname(__FILE__) . '/class-tgm-plugin-activation.php';
+
+add_action('tgmpa_register', 'my_theme_register_required_plugins');
+
+/**
+ * Register the required plugins for this theme.
+ *
+ *  <snip />
+ *
+ * This function is hooked into tgmpa_init, which is fired within the
+ * TGM_Plugin_Activation class constructor.
+ */
+function my_theme_register_required_plugins() {
+    /*
+     * Array of plugin arrays. Required keys are name and slug.
+     * If the source is NOT from the .org repo, then source is also required.
+     */
+    $plugins = array(
+        // This is an example of how to include a plugin bundled with a theme.
+        array(
+            'name' => 'Advanced custom fields pro', // The plugin name.
+            'slug' => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
+            'source' => get_stylesheet_directory() . '/lib/plugins/advanced-custom-fields-pro.zip', // The plugin source.
+            'required' => true, // If false, the plugin is only 'recommended' instead of required.
+            'version' => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+            'force_activation' => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+            'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+            'external_url' => '', // If set, overrides default API URL and points to an external URL.
+            'is_callable' => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+        )
+    );
+
+    tgmpa($plugins, $config);
+}
+
 //function add_custom_post_types() {
 //    // Add 'Events' post type
 //    $labels = array(
@@ -28,8 +66,7 @@
 //
 //add_action('init', 'add_custom_post_types');
 
-
-/** 
+/**
  * 
  * @param string $string
  * @param type $amount
